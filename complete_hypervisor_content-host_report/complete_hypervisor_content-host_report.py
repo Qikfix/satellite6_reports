d26fcea0f468b0a867f4a10e260f237fbb22e78c
Waldirio M Pinheiro <wpinheir@ironman.home>
2018-10-26 09:49:47 -0700

    working

--- complete_hypervisor_content-host_report/complete_hypervisor_content-host_report.py
+++ complete_hypervisor_content-host_report/complete_hypervisor_content-host_report.py
@@ -60,23 +60,60 @@ def process_list(hyper_list):
 
     # Hypervisor Name
     hypervisor_name = hyper_list['name']
+    print("here")
+    pass
 
     # Hypervisor Entitlement
-    try:
-        subscription_name = hyper_info['results'][0]['name'].replace(",","")
-    except IndexError:
-        subscription_name = None
 
-    if (len(hyper_detail['subscription_facet_attributes']['virtual_guests']) == 0):
-        content_host_name = None
-        ch_entitlement = None
-        # print "{},{},{},{}".format(hypervisor_name,subscription_name,content_host_name,ch_entitlement)
-        stage_list.append(hypervisor_name)
-        stage_list.append(subscription_name)
-        stage_list.append(content_host_name)
-        stage_list.append(ch_entitlement)
-        final_result.append(stage_list)
-    else:
+    guests = True
+
+    try:
+        len_result = len(hyper_info['results'])
+        subscription_name = hyper_info['results'][0]['name'].replace(",","")
+        results_is_there = True
+    except KeyError as e:
+        print("Error ... {}".format(e))
+        subscription_name = None
+        results_is_there = False
+    except IndexError as e:
+        print("Error ... {}".format(e))
+        subscription_name = None
+        results_is_there = False
+
+    try:
+        len_result = len(hyper_detail['subscription_facet_attributes']['virtual_guests'])
+        vguests_is_there = True
+    except KeyError as e:
+        print("Error ... {}".format(e))
+        # guests = False
+        vguests_is_there = False
+
+
+    # if len(hyper_info['results']) == 0:
+    #     print("{} == with zero", )
+
+    # if len(hyper_detail['subscription_facet_attributes']['virtual_guests']) == 0:
+    #     print("No VM's on {}".format(hypervisor_name))
+
+
+    # try:
+    #     subscription_name = hyper_info['results'][0]['name'].replace(",","")
+    # except IndexError as e:
+    #     print ("IndexError found - {}".format(e))
+    #     subscription_name = None
+    # except KeyError as e:
+    #     print ("KeyError found - {}".format(e))
+    #     subscription_name = None
+
+    # try:
+    #     len(hyper_detail['subscription_facet_attributes']['virtual_guests']) == 0
+    #     test_result = 0
+    # except KeyError as e:
+    #     print ("KeyError found - {}".format(e))
+    #     test_result = 0
+
+
+    if (vguests_is_there):
         for content_host in hyper_detail['subscription_facet_attributes']['virtual_guests']:
             content_host_id = content_host['id']
             content_host_name = content_host['name']
@@ -108,6 +145,59 @@ def process_list(hyper_list):
                         stage_list.append(content_host_name)
                         stage_list.append(ch_entitlement)
                         final_result.append(stage_list)
+    else:
+        content_host_name = None
+        ch_entitlement = None
+        # print "{},{},{},{}".format(hypervisor_name,subscription_name,content_host_name,ch_entitlement)
+        stage_list.append(hypervisor_name)
+        stage_list.append(subscription_name)
+        stage_list.append(content_host_name)
+        stage_list.append(ch_entitlement)
+        final_result.append(stage_list)
+
+
+
+    # if (guests == False):
+    #     content_host_name = None
+    #     ch_entitlement = None
+    #     # print "{},{},{},{}".format(hypervisor_name,subscription_name,content_host_name,ch_entitlement)
+    #     stage_list.append(hypervisor_name)
+    #     stage_list.append(subscription_name)
+    #     stage_list.append(content_host_name)
+    #     stage_list.append(ch_entitlement)
+    #     final_result.append(stage_list)
+    # else:
+    #     for content_host in hyper_detail['subscription_facet_attributes']['virtual_guests']:
+    #         content_host_id = content_host['id']
+    #         content_host_name = content_host['name']
+    #         content_host_info = get_json(SAT_API + "hosts/" + str(content_host_id) + "/subscriptions")
+
+    #         check_results = 0
+
+    #         try:
+    #             check_results = len(content_host_info['results'])
+    #         except KeyError:
+    #             check_results = -999
+
+    #         if (check_results == -999 ):
+    #             ch_entitlement = "Check This Machine"
+    #         else:
+    #             if (len(content_host_info['results']) == 0):
+    #                 ch_entitlement = None
+    #                 # print "{},{},{},{}".format(hypervisor_name,subscription_name,content_host_name,ch_entitlement)
+    #                 stage_list.append(hypervisor_name)
+    #                 stage_list.append(subscription_name)
+    #                 stage_list.append(content_host_name)
+    #                 stage_list.append(ch_entitlement)
+    #                 final_result.append(stage_list)
+    #             else:
+    #                 for ch_entitlement in content_host_info['results']:
+    #                     # print "{},{},{},{}".format(hypervisor_name,subscription_name,content_host_name,ch_entitlement['product_name'].replace(",",""))
+    #                     stage_list.append(hypervisor_name)
+    #                     stage_list.append(subscription_name)
+    #                     stage_list.append(content_host_name)
+    #                     stage_list.append(ch_entitlement)
+    #                     final_result.append(stage_list)
 
 
 def main():
@@ -127,15 +217,35 @@ def main():
         if 'virt-who' in host['name']:
             hyper_list.append(host)
     
-    thread_list = []
 
     print("3/4 - Processing all entries ...")
+    thread_list = []
     t_start = time.time()
-    for each_hyper in hyper_list:
-        t = threading.Thread(target=process_list, args=(each_hyper,))
-        t.start()
-        thread_list.append(t)
-        time.sleep(1)
+
+    total_ch = len(hosts['results'])
+    cont = 0
+
+    while True:
+        if len(thread_list) < 1:
+            print "{} == {}".format(hosts['results'][cont]['name'],cont)
+            t = threading.Thread(target=process_list, args=(hosts['results'][cont],))
+            t.start()
+            thread_list.append(t)
+            cont = cont + 1
+        else:
+            for thre in thread_list:
+                thre.join()
+            thread_list = []
+        
+        if cont == total_ch:
+            break
+
+
+    # for each_hyper in hyper_list:
+    #     t = threading.Thread(target=process_list, args=(each_hyper,))
+    #     t.start()
+    #     thread_list.append(t)
+    #     time.sleep(1)
 
     for each_thread in thread_list:
         each_thread.join()
